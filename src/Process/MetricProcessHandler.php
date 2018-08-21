4436a3e5f3c085cdd3760bdfc0ebe6341091cbae
Simon <i@wuqian.me>
2018-08-21 17:20:02 +0800

    fix

--- src/Process/MetricProcessHandler.php
+++ src/Process/MetricProcessHandler.php
@@ -27,6 +27,8 @@ class MetricProcessHandler
      */
     protected $logger;
 
+    protected $projectId;
+
     public function __construct($process, Container $container)
     {
         $this->process = $process;
@@ -34,6 +36,7 @@ class MetricProcessHandler
         $this->port = $container['metric.port'];
         $this->processManager = $container['metric.process_manager'];
         $this->logger = $container['metric.logger'];
+        $this->projectId = $container['worker.project_id'];
     }
 
     public function start()
@@ -63,6 +66,7 @@ class MetricProcessHandler
         $server->on('request', function ($request, $response) {
             $this->logger->info(json_encode($request->header).json_encode($request->server));
             if ('/metrics' === $request->server['request_uri']) {
+                $response->header('Content-Type', 'text/plain; version=0.0.4');
                 $response->end($this->createMetricsResponse());
             } else {
                 $response->end($this->createHomeResponse());
@@ -87,23 +91,102 @@ class MetricProcessHandler
 
     protected function createMetricsResponse()
     {
-        $cmd = 'ps -eo pid,stat,rss,args |grep beanworker|awk \'$0 !~ /grep/ {print $0}\'';
-        exec($cmd, $ps);
+        $cmd = 'ps -eo pid,stat,rss,etimes,args |grep \'%s beanworker\'|awk \'$0 !~ /grep/ {print $0}\'';
+        exec(sprintf($cmd, $this->projectId), $processRows);
 
-        $res = [];
-        foreach ($ps as $rowString) {
-            $arr = array_values(array_filter(explode(' ', $rowString)));
-            $row = [
-                'pid' => $arr[0],
-                'stat' => $arr[1],
-                'rss' => $arr[2],
-                'project' => $arr[3],
-                'process' => $arr[5],
-                'tube' => $arr[6] ?? '',
+        $gaugeMetrics = [
+            'up' => [],
+            'process_total' => [],
+            'process_stat' => [],
+            'process_memory_bytes' => [],
+        ];
+
+        $counterMetrics = [
+            'uptime_seconds' => [],
+        ];
+
+        $masterUp = 0;
+        $totals = [
+            'master' => 0,
+            'worker' => 0,
+            'metric' => 0,
+        ];
+        $metricProcessRssTotal = 0;
+        foreach ($processRows as $processRow) {
+            $parsed = array_values(array_filter(explode(' ', $processRow)));
+            $stat = ord(strtoupper(substr($parsed[1], 0, 1)));
+            $rss = $parsed[2];
+            $upTimes = $parsed[3];
+            $type = $parsed[6];
+            $tube = $parsed[7] ?? '';
+
+            $labels = [
+                'project' => $this->projectId,
+                'type' => $type,
             ];
-            $res[] = $row;
+
+            if ('master' === $type) {
+                $masterUp = 1;
+                $totals['master']++;
+            } else if ('worker' === $type) {
+                $labels['tube'] = $tube;
+                $totals['worker']++;
+            } else if ('metric' === $type) {
+                $metricProcessRssTotal += $rss;
+                $totals['metric']++;
+            }
+
+            if ('metric' !== $type) {
+                $gaugeMetrics['process_stat'][] = $this->createMetricItem($stat, $labels);
+                $gaugeMetrics['process_memory_bytes'][] = $this->createMetricItem($rss, $labels);
+            }
+            $counterMetrics['uptime_seconds'][] = $this->createMetricItem($upTimes, $labels);
         }
 
-        return json_encode($res);
+        $gaugeMetrics['process_memory_bytes'][] = $this->createMetricItem($metricProcessRssTotal, [
+            'project' => $this->projectId,
+            'type' => 'metric',
+        ]);
+
+        $gaugeMetrics['up'][] = $this->createMetricItem($masterUp, [
+            'project' => $this->projectId
+        ]);
+
+        foreach ($totals as $type => $total) {
+            $gaugeMetrics['process_total'][] = $this->createMetricItem($total, [
+                'project' => $this->projectId,
+                'type' => $type,
+            ]);
+        }
+
+        $content = $this->renderMetrics($gaugeMetrics, 'gauge');
+        $content .= $this->renderMetrics($counterMetrics, 'counter');
+        return $content;
+    }
+
+    protected function createMetricItem($value, array $labels = [])
+    {
+        return [
+            'labels' => $labels,
+            'value' => $value
+        ];
+    }
+
+    protected function renderMetrics($metrics, $type = 'untyped')
+    {
+        $lines = [];
+        foreach ($metrics as $name => $items) {
+            $lines[] = "# HELP {$name} statistics";
+            $lines[] = "# TYPE {$name} {$type}";
+            foreach ($items as $item) {
+                $labels = [];
+                foreach ($item['labels'] as $k => $v) {
+                    $labels[] = $k.'="'.preg_replace('/\s/', '', $v).'"';
+                }
+                $lines[] = 'beanworker_'.$name.'{'.implode(',', $labels).'} '.$item['value'];
+            }
+        }
+
+        return implode("\n", $lines) . "\n";
     }
 }
